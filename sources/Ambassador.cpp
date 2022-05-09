c3901549db26dddae6d9cd07cc1c8084cd4cbe95
Guppi4 <kirill.perevalov.1996@gmail.com>
2022-05-10 02:54:29 +0300

    17

--- sources/Ambassador.cpp
+++ sources/Ambassador.cpp
@@ -12,19 +12,34 @@ using namespace std;
     Ambassador::~Ambassador(){}
     void Ambassador::transfer( Player &from,Player &to){
        if(from.bank==0){
-           throw std::runtime_error("noot turn");
+           throw std::runtime_error("noott turn");
        }
        
         if (this->_n != this->_g->turn())
     {
-        throw std::runtime_error("noot turn");
+        throw std::runtime_error("noopt turn");
     }  
         from.bank+=-1;
         to.bank+=1;
         this->topCommand="transfer";   
        this->_g->turn2(this->_n);
     }
-    void Ambassador::block(const Player &p){}
+    void Ambassador::block( Player &p){
+        for (Player *player : p._g->objekt_players)
+        {
+            if (player->_n== p.couplast)
+            {
+             player->bank+=2;
+               p.bank+=-2;
+              // cout <<_g->index;
+                // this->_g->turn2(this->_n);
+               
+             //  cout <<_g->index;
+               break;
+                // push to array of string avtive players
+            }
+        }
+    }
     string Ambassador::role()const {
     return "Ambassador";
 }
